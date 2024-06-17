e2f0487cdcceabc222d8061ec848272ec2b58a9d
BaoBao <baobaodacmocnhi@gmail.com>
2024-06-17 15:19:36 +0700

    u

--- WSTanHoa/WSTanHoa/Controllers/apiSWOCController.cs
+++ WSTanHoa/WSTanHoa/Controllers/apiSWOCController.cs
@@ -11,6 +11,7 @@ using WSTanHoa.Providers;
 
 namespace WSTanHoa.Controllers
 {
+    [RoutePrefix("api/SWOC")]
     public class apiSWOCController : ApiController
     {
         private CConnection _cDAL_DHN = new CConnection(CGlobalVariable.DHN);
@@ -33,17 +34,30 @@ namespace WSTanHoa.Controllers
             return result;
         }
 
-        [Route("")]
+        [Route("ThongKe_KhieuNai")]
         [HttpGet]
         public MResult ThongKe_KhieuNai(string CreateDate)
         {
             MResult result = new MResult();
             try
             {
-                DataTable dt = _cDAL_DHN.ExecuteQuery_DataTable("select * from KTKS_DonKH.dbo.DonTu dt,KTKS_DonKH.dbo.DonTu_ChiTiet dtct
-where CAST(dt.CreateDate as date) >= '20240101' and dt.MaDon = dtct.MaDon
-and(select count(*) from KTKS_DonKH.dbo.DonTu_ChiTiet where KTKS_DonKH.dbo.DonTu_ChiTiet.MaDon = dt.MaDon) = 1 and Name_NhomDon_PKH not like ''
-and(select stuff((select ';' + cast(NameGroup as nvarchar(100)) FROM[KTKS_DonKH].[dbo].[NhomDon] where KhieuNai = 1 group by NameGroup for xml path('')),1,1,'')) like '%' + Name_NhomDon_PKH + '%'");
+                string[] dates = CreateDate.Split('/');
+                DataTable dt = _cDAL_DHN.ExecuteQuery_DataTable("declare @year int=" + dates[2] + ";"
+                    + " declare @month int = " + dates[1] + ";"
+                    + " declare @day int= " + dates[0] + ";"
+                    + " with luyke as"
+                    + " (select dtct.* from KTKS_DonKH.dbo.DonTu dt,KTKS_DonKH.dbo.DonTu_ChiTiet dtct"
+                    + "  where YEAR(dt.CreateDate) = @year and month(dt.CreateDate) >= 1 and  month(dt.CreateDate) < @month and dt.MaDon = dtct.MaDon"
+                    + "  and(select count(*) from KTKS_DonKH.dbo.DonTu_ChiTiet where KTKS_DonKH.dbo.DonTu_ChiTiet.MaDon = dt.MaDon) = 1 and Name_NhomDon_PKH not like ''"
+                    + "  and(select stuff((select ';' + cast(NameGroup as nvarchar(100)) FROM[KTKS_DonKH].[dbo].[NhomDon] where KhieuNai = 1 group by NameGroup for xml path('')),1,1,'')) like '%' + Name_NhomDon_PKH + '%'),"
+                    + "   hientai as"
+                    + " (select dtct.* from KTKS_DonKH.dbo.DonTu dt,KTKS_DonKH.dbo.DonTu_ChiTiet dtct"
+                    + "  where YEAR(dt.CreateDate) = @year and month(dt.CreateDate) = @month and dt.MaDon = dtct.MaDon"
+                    + "  and(select count(*) from KTKS_DonKH.dbo.DonTu_ChiTiet where KTKS_DonKH.dbo.DonTu_ChiTiet.MaDon = dt.MaDon) = 1 and Name_NhomDon_PKH not like ''"
+                    + "  and(select stuff((select ';' + cast(NameGroup as nvarchar(100)) FROM[KTKS_DonKH].[dbo].[NhomDon] where KhieuNai = 1 group by NameGroup for xml path('')),1,1,'')) like '%' + Name_NhomDon_PKH + '%')"
+                    + "  select ma_don_vi = 'TH', ten_don_vi = N'Tân Hòa',nam = @year,thang = @month,so_khkn_tn = (select count(*) from hientai where DAY(CreateDate) = @day)"
+                    + "  ,xu_ly_trong_thang = (select count(*) from hientai where HoanThanh = 1),so_khkn_trong_thang = (select count(*) from hientai),chua_xu_ly = (select count(*) from hientai where HoanThanh = 0)"
+                    + "  ,khkn_luy_ke = (select count(*) from luyke),thang_luy_ke = (select @month - 1),xu_ly_tn = (select count(*) from hientai where DAY(CreateDate) = @day and HoanThanh = 1),chi_tieu = null,ty_le = null,xu_ly_luy_ke = (select count(*) from luyke where HoanThanh = 1)");
                 result.data = JsonConvert.SerializeObject(dt);
                 result.success = true;
             }
@@ -61,7 +75,20 @@ and(select stuff((select ';' + cast(NameGroup as nvarchar(100)) FROM[KTKS_DonKH]
             MResult result = new MResult();
             try
             {
-                DataTable dt = _cDAL_DHN.ExecuteQuery_DataTable("");
+                string[] dates = CreateDate.Split('/');
+                DataTable dt = _cDAL_DHN.ExecuteQuery_DataTable("declare @year int=" + dates[2] + ";"
+                    + " declare @month int = " + dates[1] + ";"
+                    + " declare @day int= " + dates[0] + ";"
+                    + " with luyke as"
+                    + " (select NGAYGIAITRACH, MaNV_DangNgan, GIABAN from HOADON_TA.dbo.HOADON where Nam= @year and Ky< @month),"
+                    + "   hientai as"
+                    + " (select NGAYGIAITRACH, MaNV_DangNgan, GIABAN from HOADON_TA.dbo.HOADON where Nam= @year and Ky = @month)"
+                    + "  select ma_don_vi = 'TH', ten_don_vi = N'Tân Hòa',nam = @year,thang = @month,sohd_thu_tn = (select count(*) from hientai where MaNV_DangNgan is not null and YEAR(NGAYGIAITRACH) = @year and month(NGAYGIAITRACH) = @month and day(NGAYGIAITRACH) = @day)"
+                    + " , sohd_ton = (select count(*) from hientai where MaNV_DangNgan is null or NGAYGIAITRACH is null),gia_ban_bq = (select GiaBanBinhQuan from HOADON_TA.dbo.TT_GiaBanBinhQuan where Nam = @year and Ky = @month),thang_luy_ke = @month - 1"
+                    + " , thu_luy_ke = (select count(*) from luyke where MaNV_DangNgan is not null and YEAR(NGAYGIAITRACH) = @year and month(NGAYGIAITRACH) = @month and day(NGAYGIAITRACH) = @day)"
+                    + " , thu_trong_thang = (select count(*) from hientai where MaNV_DangNgan is not null and YEAR(NGAYGIAITRACH) = @year and month(NGAYGIAITRACH) = @month)"
+                    + " , tien_thu_tn = (select sum(giaban) from hientai where MaNV_DangNgan is not null and YEAR(NGAYGIAITRACH) = @year and month(NGAYGIAITRACH) = @month and day(NGAYGIAITRACH) = @day)"
+                    + " , chi_tieu = (select DoanhThu from TRUNGTAMKHACHHANG.dbo.BC_KeHoach where Nam = @year),ty_le = (select sum(giaban) from hientai where MaNV_DangNgan is not null and YEAR(NGAYGIAITRACH) = @year and month(NGAYGIAITRACH) = @month and day(NGAYGIAITRACH) = @day)/ (select DoanhThu from TRUNGTAMKHACHHANG.dbo.BC_KeHoach where Nam = @year)*100");
                 result.data = JsonConvert.SerializeObject(dt);
                 result.success = true;
             }
@@ -146,7 +173,7 @@ and(select stuff((select ';' + cast(NameGroup as nvarchar(100)) FROM[KTKS_DonKH]
 
         [Route("")]
         [HttpGet]
-        public MResult ThongKe_SanLuong(string Thang,string Nam)
+        public MResult ThongKe_SanLuong(string Thang, string Nam)
         {
             MResult result = new MResult();
             try
--- WSTanHoa/WSTanHoa/info.aspx.cs
+++ WSTanHoa/WSTanHoa/info.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WSTanHoa.Providers;
 
 namespace WSTanHoa
 {
@@ -11,34 +13,38 @@ namespace WSTanHoa
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            switch (Request.QueryString["id"])
+            if (Request.QueryString.Count > 0 && Request.QueryString.AllKeys.Contains("id") && Request.QueryString["id"].ToString() != "")
             {
-                case "204":
-                    lbID.Text = Request.QueryString["id"];
-                    lbHoTen.Text = "Lê Tấn Đạt";
-                    lbChucVu.Text = "Phó Trưởng Phòng KTCN";
-                    lbDienThoai.Text = "0909933397";
-                    lbEmail.Text = "letandat@capnuoctanhoa.com.vn";
-                    Image1.ImageUrl = "Images/ava240.jpg";
-                    break;
-                case "312":
-                    lbID.Text = Request.QueryString["id"];
-                    lbHoTen.Text = "Nguyễn Ngọc Quốc Bảo";
-                    lbChucVu.Text = "Nhân Viên Phòng KTCN";
-                    lbDienThoai.Text = "0938040301";
-                    lbEmail.Text = "quocbao@capnuoctanhoa.com.vn";
-                    Image1.ImageUrl = "Images/ava312.jpg";
-                    break;
-                default:
-                    lbID.Text = "ID";
+                CConnection _cDAL_TTKH = new CConnection(CGlobalVariable.TrungTamKhachHang);
+                DataTable dt = _cDAL_TTKH.ExecuteQuery_DataTable("select * from NhanVien where MaNV='" + Request.QueryString["id"] + "'");
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    lbID.Text = dt.Rows[0]["MaNV"].ToString();
+                    lbHoTen.Text = dt.Rows[0]["HoTen"].ToString();
+                    lbChucVu.Text = dt.Rows[0]["ChucDanh"].ToString();
+                    lbDienThoai.Text = dt.Rows[0]["DienThoai"].ToString();
+                    lbEmail.Text = dt.Rows[0]["Email"].ToString();
+                    Image1.ImageUrl = "Images/NhanVien/" + dt.Rows[0]["MaNV"].ToString() + ".jpg";
+                }
+                else
+                {
+                    lbID.Text = "MaNV";
                     lbHoTen.Text = "Họ Tên";
                     lbChucVu.Text = "Chức Vụ";
                     lbDienThoai.Text = "Điện Thoại";
                     lbEmail.Text = "Email";
                     Image1.ImageUrl = "Images/logoctycp.png";
-                    break;
+                }
             }
-            
+            else
+            {
+                lbID.Text = "MaNV";
+                lbHoTen.Text = "Họ Tên";
+                lbChucVu.Text = "Chức Vụ";
+                lbDienThoai.Text = "Điện Thoại";
+                lbEmail.Text = "Email";
+                Image1.ImageUrl = "Images/logoctycp.png";
+            }
         }
     }
 }
